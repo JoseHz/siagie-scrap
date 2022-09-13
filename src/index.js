575be48a21e3880a15d04df6628df5edb93da6b9
Jose Pineda <solware.eirl@gmail.com>
2022-09-13 10:49:38 -0500

    mejorando la logica

--- src/index.js
+++ src/index.js
@@ -9,25 +9,27 @@ const data = [];
   const browser = await chromium.launch()
   const page = await browser.newPage()
   await page.goto('http://sistemas10.minedu.gob.pe/siagie3/Login.aspx');
-  await page.locator('#imgCaptcha').screenshot({ path: `./siagie.png` });
-  await displayImage.fromFile("siagie.png").then(image => {
+  await page.locator('#imgCaptcha').screenshot({ path: `./output/capcha/lastCaptcha.png` });
+  await displayImage.fromFile("./output/capcha/lastCaptcha.png").then(image => {
     console.log(image)
   })
   
+  //iniciop sesion
   await prompt.start();
-  const {userName, password, segCaptcha} = await prompt.get(['userName', 'password', 'segCaptcha']);
-  await console.log(userName, password, segCaptcha);  
-  
-  await console.log(data);
+  const userName = '07524459';
+  const password = 'kOb4Kq?]ygX&';
+  const {segCaptcha} = await prompt.get(['segCaptcha']);
   await page.fill('input[id="txtUsuario"]', userName);
   await page.fill('input[id="txtFraseSecreta"]',  password);
   await page.fill('input[id="txtCaptcha"]',segCaptcha.toUpperCase());
   await page.click('.btn-enviar');
-  await page.screenshot({ path: `./1.png` });
-  await page.screenshot({ path: `./2.png` });
-  await page.screenshot({ path: `./3.png` });
-  await page.screenshot({ path: `./4.png` });
-  await page.screenshot({ path: `./5.png` });
+
+  //entramos al sistema
+  page.on('modal-content', dialog => dialog.dismiss())
+  await page.locator('#btnCerrarModalAutorizacion').click();
+
+  //tomamos captura de pantalla
+  await page.screenshot({ path: `./output/screenShot/${Date.now().toString(36)}.png` });
   await browser.close()
 })()
 
